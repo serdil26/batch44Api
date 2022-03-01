968953fc07153986d73946eb8ed993275cd28308
Sezgin Erdil <sezginerdil@gmail.com>
2022-03-01 22:14:26 +0100

    First commit of Api

--- src/test/java/get_http_request/C01_GetRequest.java
+++ src/test/java/get_http_request/C01_GetRequest.java
@@ -16,17 +16,18 @@ public class C01_GetRequest {
         //response.prettyPrint();
 
         System.out.println("status code =" + response.statusCode());
+        System.out.println("status line= " + response.statusLine());
         System.out.println("content type= " + response.contentType());
         System.out.println("test suresi= " + response.time());
 
         Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals("application/json; charset=utf-8", response.contentType());
+
+        response.
+                then().assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8");
 
     }
-
-
-
-
-
-
 
 }
