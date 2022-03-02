fb54ccf47a9284e6f3b267f184ed7507a784d778
Sezgin Erdil <sezginerdil@gmail.com>
2022-03-02 20:25:27 +0100

    First commit of Api

---
+++ src/test/java/get_http_request/C02_GetRequest.java
@@ -0,0 +1,42 @@
+package get_http_request;
+
+
+import io.restassured.response.Response;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class C02_GetRequest {
+    @Test
+    public void test02(){
+        String url= "https://reqres.in/api/users";
+
+        Response response= given().when().get(url);
+        response.prettyPrint();    //response'daki body'i getirir
+        /*response.prettyPeek();      //response'daki herseyi getirir
+        response.then().log().all(); //response'daki herseyi getirir*/
+
+        response.then()
+                .assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .statusLine("HTTP/1.1 200 OK");
+        //body control
+        response.then().
+                body("data[0].first_name", equalTo("George"),
+                        "data[0].last_name", equalTo("Bluth"),
+                        "data[0].email", equalTo("george.bluth@reqres.in"));
+
+        response.then().body("data[1].id", equalTo(2),
+                "data[1].email",equalTo("janet.weaver@reqres.in"),
+                "data[1].first_name", equalTo("Janet"),
+                "data[1].last_name",equalTo("Weaver"),
+                "data[1].avatar", equalTo("https://reqres.in/img/faces/2-image.jpg"));
+
+
+
+    }
+}
